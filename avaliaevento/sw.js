25550fc23cd219030b7ec42400e31226afb7f5bd
Luca Jara <jaraluca@gmail.com>
2024-11-26 21:23:48 -0400

    Estrutura base do código
    Estrutura base do código para teste utilizando github pages

---
+++ avaliaevento/sw.js
@@ -0,0 +1,31 @@
+// sw.js
+const CACHE_NAME = 'avaliacao-eventos-cache-v1';
+const urlsToCache = [
+  'index.html',
+  'style.css',
+  'script.js',
+  'manifest.json',
+  // Outros arquivos estáticos...
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache aberto:', CACHE_NAME);
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
